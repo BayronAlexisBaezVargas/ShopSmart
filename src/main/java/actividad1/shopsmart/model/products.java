3842b34ae73f692152378fec6edbc5e55085daa0
bayron <bay.baez@duocuc.cl>
2026-03-10 11:24:02 -0300

    Modelo creado

---
+++ src/main/java/actividad1/shopsmart/model/products.java
@@ -0,0 +1,69 @@
+package actividad1.shopsmart.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private Double precio;
+
+    @Column
+    private Integer Stock;
+
+    @Column
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return Stock;
+    }
+
+    public void setStock(Integer Stock) {
+        this.Stock = Stock;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
