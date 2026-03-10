4e0a540c8ebaca2487802a616ba8ecf5ffbfe674
bayron <bay.baez@duocuc.cl>
2026-03-10 10:49:56 -0300

    Commit inicial

---
+++ src/test/java/actividad1/shopsmart/ShopSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package actividad1.shopsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
